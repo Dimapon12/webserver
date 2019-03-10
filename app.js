17ea523942716a1bf7eb34b1ecbfc59052da804a
dmitry <dimapon12@gmail.com>
2019-03-10 18:18:47 +0200

    4-12

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var lab1Router = require('./routes/lab1');
+var lab4_12Router = require('./routes/lab4-12');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/lab1', lab1Router);
+app.use('/lab4-12', lab4_12Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/lab4-12.js
@@ -0,0 +1,160 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var htmlpath = require('../public/htmlpages')
+
+router.get('/6.33', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/6/6.33.html'));
+});
+
+router.get('/6.34', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/6/6.34.html'));
+});
+
+router.get('/8.1', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/11.1.html'));
+});
+
+router.get('/8.2', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.2.html'));
+});
+
+router.get('/8.3', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.3.html'));
+});
+
+router.get('/8.4', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.4.html'));
+});
+
+router.get('/8.5', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.5.html'));
+});
+
+router.get('/8.6', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.6.html'));
+});
+
+router.get('/8.7', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.7.html'));
+});
+
+router.get('/8.8', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.8.html'));
+});
+
+router.get('/8.9', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/8/8.9.html'));
+});
+
+router.get('/9.1', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/9/9.1.html'));
+});
+
+router.get('/9.2', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/9/9.2.html'));
+});
+
+router.get('/9.3', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/9/9.3.html'));
+});
+
+router.get('/9.4', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/9/9.4.html'));
+});
+
+router.get('/9.5', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/9/9.5.html'));
+});
+
+router.get('/10.1', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/10/10.1.html'));
+});
+
+router.get('/10.2', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/10/10.2.html'));
+});
+
+router.get('/10.3', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/10/10.3.html'));
+});
+
+router.get('/10.4', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/10/10.4.html'));
+});
+
+router.get('/10.5', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/10/10.5.html'));
+});
+
+
+
+router.get('/11.1', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.1.html'));
+});
+
+router.get('/11.2', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.2.html'));
+});
+
+router.get('/11.3', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.3.html'));
+});
+
+router.get('/11.4', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.4.html'));
+});
+
+router.get('/11.5', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.5.html'));
+});
+
+router.get('/11.6', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.6.html'));
+});
+
+router.get('/11.7', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.7.html'));
+});
+
+router.get('/11.8', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.8.html'));
+});
+
+router.get('/11.9', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/11/11.9.html'));
+});
+
+
+router.get('/12.1', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.1.html'));
+});
+
+router.get('/12.2', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.2.html'));
+});
+
+router.get('/12.3', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.3.html'));
+});
+
+router.get('/12.4', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.4.html'));
+});
+
+router.get('/12.5', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.5.html'));
+});
+
+router.get('/12.6', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.6.html'));
+});
+
+router.get('/12.7', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.7.html'));
+});
+
+router.get('/12.8', function(req, res, next) {
+  res.sendFile(path.join(htmlpath + '/12/12.8.html'));
+});
+module.exports = router;
